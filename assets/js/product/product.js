55558056054267010b336eae1c89f27a1ab94709
naslanzade <novrastafa@code.edu.az>
2023-03-21 22:54:51 +0400

    add contact, product, blog pages

---
+++ assets/js/product/product.js
@@ -0,0 +1,82 @@
+$(document).ready(function () {
+   
+    let basketIcon = document.querySelectorAll(".add-basket");
+    let basket = [];
+
+
+    if(JSON.parse(localStorage.getItem("basket"))!=null){
+        basket = JSON.parse(localStorage.getItem("basket")) 
+    }
+     
+
+    basketIcon.forEach(btn => {
+        btn.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            let productPrice = this.parentNode.nextElementSibling.nextElementSibling.nextElementSibling.firstElementChild.innerText;
+            let productName = this.parentNode.nextElementSibling.innerText;
+            let productImg = this.parentNode.previousElementSibling.previousElementSibling.previousElementSibling.getAttribute("src");
+            let productId = parseInt(this.closest(".card").getAttribute("data-id"))
+
+            console.log(productId)
+
+            let existProduct = basket.find(m => m.id == productId);            
+
+            if (existProduct != undefined) {
+                existProduct.count += 1;
+            } else {
+                basket.push({
+
+                    id: productId,
+                    name: productName,
+                    price: productPrice,
+                    image: productImg,
+                    count: 1
+                })
+            }
+            localStorage.setItem("basket", JSON.stringify(basket));
+            getBasketCount(basket)
+        })
+
+
+    });
+
+    getBasketCount(basket);
+
+    function getBasketCount(arr) {
+        let count = 0;
+        for (const item of arr) {
+            count += item.count
+        }
+        document.querySelector(".cart span").innerText = count;
+    }
+
+
+    $(".image .item").click(function () {
+
+        $(".active-tab").removeClass("active-tab")
+        $(this).addClass("active-tab")
+
+
+        let elements = $(".main-image .image-tab")
+        for (const content of $(elements)) {
+            if ($(this).attr("data-id") == $(content).attr("data-id")) {
+                $(content).removeClass("d-none")
+            }
+            else {
+                $(content).addClass("d-none")
+            }
+        }
+
+    })
+
+
+
+
+
+
+
+    
+
+
+})
