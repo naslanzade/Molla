3904bfa9777a885987b06f67825e1d3e3a2694a1
naslanzade <novrastafa@code.edu.az>
2023-03-24 18:06:08 +0400

    add changes to cart page

--- assets/js/cart/cart.js
+++ assets/js/cart/cart.js
@@ -49,30 +49,39 @@ $(document).ready(function () {
         document.querySelector(".favorite .wish").innerText = count;
     }
 
+
+
+
     let tableBody = document.querySelector("tbody");
-    let alert = document.querySelector(".icon");
-    let input = document.querySelectorAll("input")
-    let totalPrice=document.querySelector(".total-value")
+    let alert = document.querySelector(".icon");    
+    let totalPrice = document.querySelector(".total-value")
     let products = JSON.parse(localStorage.getItem("basket"))
 
-    if (products != null) {
 
+    if (totalPrice.innerText == "") {
+        totalPrice.innerText = "$ 0"
+    }
+
+    totalPrice.innerText = `$ ${price(products)}`
+
+
+    if (products != null) {
         products.forEach(product => {
             tableBody.innerHTML += `<tr data-id="${product.id}">
             <td><img src="${product.image}" alt=""></td>
             <td class="name">${product.name}</td>
             <td class="price">${product.price}</td>
-            <td>
-            <div class="count">
-            <i class="fa-solid fa-minus minus"></i>
-            <input type="number" value="${product.count}" min="1" max="10" disabled>
-            <i class="fa-solid fa-plus plus"></i>
-            <td class="-total">${parseInt(product.price.replace("$", "")) * product.count} $</td>
+            <td class="product-count" data-id = ${product.id}>
+            <span class="minus" data-id="${product.id}">-</span>
+            <span class="count" data-id=${product.id}>${product.count}</span>
+            <span class="plus" data-id="${product.id}">+</span>
+            </td>
+            <td class="-total">$ ${parseInt(product.price.replace("$", "")) * product.count}</td>
             <td><i class="fa-solid fa-xmark"></i></td>        
         </tr>`
 
         });
-    } else{
+    } else {
         tableBody.parentNode.classList.add("d-none")
         alert.classList.remove("d-none")
     }
@@ -83,44 +92,66 @@ $(document).ready(function () {
     }
 
 
-  
+
     let deleteIcon = document.querySelectorAll(".fa-xmark")
 
     deleteIcon.forEach(btn => {
 
-        btn.addEventListener("click",function(){
-            let id=parseInt(this.parentNode.parentNode.getAttribute("data-id"))
-            products=JSON.parse(localStorage.getItem("basket"))
-            let result=products.filter(m=>m.id!=id)
+        btn.addEventListener("click", function () {
+            let id = parseInt(this.parentNode.parentNode.getAttribute("data-id"))
+            products = JSON.parse(localStorage.getItem("basket"))
+            let result = products.filter(m => m.id != id)
 
-            localStorage.setItem("basket",JSON.stringify(result));
+            localStorage.setItem("basket", JSON.stringify(result));
             this.parentNode.parentNode.remove();
             getProductCount(result)
         })
-        
+
     });
 
 
-    
+    let decreaseButtons = document.querySelectorAll(".minus")
+    let increaseButtons = document.querySelectorAll(".plus")
+    let count = document.querySelectorAll(".count");
 
 
 
+    decreaseButtons.forEach(btn => {
+        btn.addEventListener("click", function () {
+            let product = products.find(m => m.id == btn.getAttribute("data-id"))
+            if (product.count > 1) {
+                product.count--;
+                localStorage.setItem("basket", JSON.stringify(products))
+
+                for (const count of countProduct) {
+                    if (count.getAttribute("data-id") == btn.getAttribute("data-id")) {
+                        count.innerText = count.innerText - 1
+                    }
+                }
+            }
+
+            count.innerText = getBasketCount(JSON.parse(localStorage.getItem("basket")))
 
 
+        })
+    });
 
 
+    increaseButtons.forEach(btn => {
+        btn.addEventListener("click", function () {
+            let product = products.find(m => m.id == btn.getAttribute("data-id"))
+            product.count++;
+            localStorage.setItem("basket", JSON.stringify(products))
 
+            for (const count of countProduct) {
+                if (count.getAttribute("data-id") == btn.getAttribute("data-id")) {
+                    count.innerText = parseInt(count.innerText) + 1
+                }
+            }
 
-
-
-
-
-
-
-
-
-
-
+            count.innerText = getBasketCount(JSON.parse(localStorage.getItem("basket")))
+        })
+    });
 
 
 
@@ -138,7 +169,7 @@ $(document).ready(function () {
     function price(products) {
         let sum = 0;
         for (const product of products) {
-            sum += parseInt(product.price.replace("$",""))*product.count;
+            sum += parseInt(product.price.replace("$", "")) * product.count;
         }
         return sum;
     }
